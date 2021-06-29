8d8884bad13e2c5c6b8287a6712469733d242828
Gohar <gpkeshishyan@gmail.com>
2021-06-29 08:45:11 +0400

    updated

--- Project/redteamproject/blog/admin.py
+++ Project/redteamproject/blog/admin.py
@@ -5,6 +5,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
     list_filter = ("status",)
     search_fields = ['title', 'content']
-    prepopulated_fields = {'slug': ('title',)}
+    # prepopulated_fields = {'slug': ('title'),}
 
 admin.site.register(Post, PostAdmin)
--- Project/redteamproject/redteamproject/settings.py
+++ Project/redteamproject/redteamproject/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIRS = os.path.join(BASE_DIR,'blog/templates')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'redteamproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIRS],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
